cc12ff4bd45cda6e1aae52f02c13dc4894ed3527
ASLawan <nawalnitsua@gmail.com>
2023-12-28 14:25:11 +0100

    Write a bash script that concatenates two ore more strings

---
+++ scripts/concatenate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# script that concatenates strings
+
+VAR1="Hello"
+VAR2="Lawan!"
+VAR3="How are you doing?"
+VAR4="${VAR1} ${VAR2} ${VAR3}"
+
+echo "$VAR1"
+echo "$VAR2"
+echo "$VAR3"
+echo "$VAR4"
