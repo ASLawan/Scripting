0bd217dd7082729b10051a2783fbfee79fbb80a1
ASLawan <nawalnitsua@gmail.com>
2023-12-28 01:59:50 +0100

    Learn how to declare and use variabes

---
+++ edureka/variable.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#######################
+#
+# Author: Lawan
+# Date: 27 - 12 - 2023
+#
+# Script to learn about variables
+#
+###########################
+
+NAME="Lawan"
+
+echo "Hello $NAME"
+echo 
+echo "Special variables"
+echo "\$0 - represents file name"
+echo
+echo "\$# - represents number of command line arguments"
+echo
+echo "\$* - represents double-quoted arguments"
+echo
+echo "\$@ - represnts single-word double-quoted arguments"
+echo
+echo "\$? - represents exit status"
+echo
+echo "\${1-9} - represents individual cmd line arguments"
+echo
+echo "\$$ - represents processs ID"
