98977bf5c4a1399da75dad530b56ee8fe8339bf4
ASLawan <nawalnitsua@gmail.com>
2023-12-28 02:03:52 +0100

    Write a function that prints greetings

---
+++ edureka/for_loop/functions/greet.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+####################
+# Author: Lawan
+# Date: 27-12-2023
+#
+# Script to implement a function that greets
+#
+####################
+
+Hello()
+{
+	echo "Hello Learner!"
+}
+
+Hello
