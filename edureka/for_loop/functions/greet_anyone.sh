73f21392ca6dfe22ac9f2b8e3671419b2259544f
ASLawan <nawalnitsua@gmail.com>
2023-12-28 02:04:32 +0100

    Modify function to greet each user by name

---
+++ edureka/for_loop/functions/greet_anyone.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#####################
+# Author: Lawan
+# Date: 27-12-2023
+#
+# implement a function that greets anyone
+#
+#####################
+
+Hello()
+{
+	echo "Hello $1!"
+}
+
+Hello Lawan
