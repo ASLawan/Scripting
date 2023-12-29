6b948713f3f9c9b999b5a3f64dcdd2f0ff4c876c
ASLawan <nawalnitsua@gmail.com>
2023-12-29 21:49:05 +0100

    Write a script that prints datea in different formats

---
+++ scripts/print_date.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# script to print date in different formats
+
+DATE1=$(date +%F)
+echo "$DATE1"
+echo
+DATE2=$(date +%D)
+echo "$DATE2"
+echo
+DATE3=$(date +%Y)
+echo "$DATE3"
+
